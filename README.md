45e998e01eb8a8f0dd5cb65201d0b0f643ed28ae
Truong Nguyen <samuraitruong@hotmail.com>
2022-04-29 16:59:23 +1000

    feat: basic initial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # last-commit-action
+
 A simple action to return last commit id for each package in workspace
+
+## Usage
+
+```
+- uses: samuraitruong/last-commit-action@main
+  id: last-commit-action
+  with:
+    package-manager: 'yarn'
+    output: 'env'
+```
---
+++ src/index.js
@@ -0,0 +1,50 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { execSync } = require("child_process");
+try {
+  const inputs = ["output", "package-manager"];
+  const [output = "env", packageManager = "yarn"] = inputs.map((prop) =>
+    core.getInput(prop)
+  );
+
+  if (packageManager !== "yarn") {
+    throw new Error(`package-manager must be yarn`);
+  }
+
+  const exportLastCommitSha = (key, directory) => {
+    const value = execSync(`git log --pretty=tformat:"%h" -n1 ${directory}`)
+      .toString()
+      .trim();
+    if (output === "env") {
+      core.exportVariable(key, value);
+    } else {
+      core.setOutput(key, branch);
+    }
+    core.info(`${output} ->  ${key}=${value}`);
+  };
+
+  const packages = execSync("yarn workspaces list --json")
+    .toString()
+    .trim()
+    .split("\n")
+    .map((x) => {
+      return JSON.parse(x);
+    });
+
+  packages.forEach(({ name, location }) => {
+    exportLastCommitSha(name.split("/").pop(), location);
+  });
+
+  const workspaces =
+    JSON.parse(fs.readFileSync("package.json", "utf8")).workspaces || [];
+
+  if (packageJson) {
+    workspaces.forEach((workspace) => {
+      const rootFolder = workspace.split("/")[0];
+
+      exportLastCommitSha(rootFolder, "./" + rootFolder);
+    });
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
